9e04db27eff8dc0f1a1b336a57d096a4a191fcd7
gyanAsh <gyan.ekka@mathongo.com>
2025-02-24 20:28:57 +0530

    chore (.evn): added DENO .env

---
+++ GmailOAuth/index.ts
@@ -0,0 +1,8 @@
+import { google } from "npm:googleapis";
+
+// Gmail API OAuth 2.0 configuration
+const oauth2Client = new google.auth.OAuth2(
+  Deno.env.get("CLIENT_ID"),
+  Deno.env.get("CLIENT_SECRET"),
+  "http://localhost:3000/auth/callback" // Your redirect URI
+);
