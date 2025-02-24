9c226b3ae0858320d0b4fa546554f125d43b4123
gyanAsh <gyan.ekka@mathongo.com>
2025-02-24 17:22:58 +0530

    feat (oak): added oak to deno project

--- main.ts
+++ main.ts
@@ -1,8 +1,28 @@
+import { Application, Context, Router, route } from "@oak/oak";
+
+const router = new Router();
+
+router
+  .get("/", (ctx) => {
+    ctx.response.body = "Hello world";
+  })
+  .get("/users", (ctx: Context) => {
+    ctx.response.body = "this is users";
+  });
+router.get(
+  "/users/:id",
+  route((req, ctx) => {
+    console.log(ctx.params.id);
+    return Response.json({ title: "this is user's id", id: ctx.params.id });
+  })
+);
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
+
 export function add(a: number, b: number): number {
   return a + b;
-}
-
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
 }
