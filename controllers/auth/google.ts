a75ca741a87c5d6014544b2498a9ef0a20c4374d
gyanAsh <gyanashekka1738@gmail.com>
2025-02-27 22:49:05 +0530

    update (scope): google-api scope updated

--- controllers/auth/google.ts
+++ controllers/auth/google.ts
@@ -25,6 +25,9 @@ export const googleAuth = async (ctx: Context) => {
 
     // Specify the scopes you want (Google accepts scopes as space-separated strings)
     const scopes = [
+      "openid",
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
       "https://www.googleapis.com/auth/gmail.modify",
       "https://www.googleapis.com/auth/gmail.send",
       "https://www.googleapis.com/auth/gmail.compose", // optional
