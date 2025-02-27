6f7de15bfd10b1bc57d6d8f2621052db8aa44827
gyanAsh <gyanashekka1738@gmail.com>
2025-02-27 22:35:25 +0530

    chore (clean-up): removed unused code

--- GmailOAuth/index.ts
+++
@@ -1,8 +0,0 @@
-import { google } from "npm:googleapis";
-
-// Gmail API OAuth 2.0 configuration
-export const oauth2Client = new google.auth.OAuth2(
-  Deno.env.get("CLIENT_ID"),
-  Deno.env.get("CLIENT_SECRET"),
-  "http://localhost:3000/auth/callback" // Your redirect URI
-);
